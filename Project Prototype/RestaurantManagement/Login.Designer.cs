8adeb5dc243e8a4db2c03a13abf7af2a386d500b
LaibaFatimaKhan <LaibaFatimaKhan@users.noreply.github.com>
2019-11-14 12:41:19 +0500

    form1 updated
    Form1 updated

--- Project Prototype/RestaurantManagement/Login.Designer.cs
+++ Project Prototype/RestaurantManagement/Login.Designer.cs
@@ -28,78 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.label_UserName = new System.Windows.Forms.Label();
+            this.textbox_UserName = new System.Windows.Forms.TextBox();
+            this.textbox_Password = new System.Windows.Forms.TextBox();
+            this.label_Password = new System.Windows.Forms.Label();
+            this.button_Login = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.button_Register = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // label_UserName
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(246, 137);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(116, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Username:";
+            this.label_UserName.AutoSize = true;
+            this.label_UserName.Location = new System.Drawing.Point(112, 104);
+            this.label_UserName.Name = "label_UserName";
+            this.label_UserName.Size = new System.Drawing.Size(75, 19);
+            this.label_UserName.TabIndex = 0;
+            this.label_UserName.Text = "Username:";
+            this.label_UserName.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // textbox_UserName
             // 
-            this.textBox1.Location = new System.Drawing.Point(390, 137);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(167, 31);
-            this.textBox1.TabIndex = 1;
+            this.textbox_UserName.Location = new System.Drawing.Point(203, 99);
+            this.textbox_UserName.Name = "textbox_UserName";
+            this.textbox_UserName.Size = new System.Drawing.Size(217, 26);
+            this.textbox_UserName.TabIndex = 1;
+            this.textbox_UserName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
-            // textBox2
+            // textbox_Password
             // 
-            this.textBox2.Location = new System.Drawing.Point(390, 195);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(167, 31);
-            this.textBox2.TabIndex = 3;
+            this.textbox_Password.Location = new System.Drawing.Point(203, 143);
+            this.textbox_Password.Name = "textbox_Password";
+            this.textbox_Password.Size = new System.Drawing.Size(217, 26);
+            this.textbox_Password.TabIndex = 3;
             // 
-            // label2
+            // label_Password
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(250, 195);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 25);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Password:";
+            this.label_Password.AutoSize = true;
+            this.label_Password.Location = new System.Drawing.Point(115, 148);
+            this.label_Password.Name = "label_Password";
+            this.label_Password.Size = new System.Drawing.Size(71, 19);
+            this.label_Password.TabIndex = 2;
+            this.label_Password.Text = "Password:";
             // 
-            // button1
+            // button_Login
             // 
-            this.button1.Location = new System.Drawing.Point(345, 275);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 35);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "Login";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button_Login.Location = new System.Drawing.Point(203, 206);
+            this.button_Login.Name = "button_Login";
+            this.button_Login.Size = new System.Drawing.Size(92, 39);
+            this.button_Login.TabIndex = 4;
+            this.button_Login.Text = "Login";
+            this.button_Login.UseVisualStyleBackColor = true;
+            this.button_Login.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(166, 21);
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(111, 34);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(482, 37);
+            this.label3.Size = new System.Drawing.Size(312, 27);
             this.label3.TabIndex = 5;
             this.label3.Text = "Restaurant Management System";
             this.label3.Click += new System.EventHandler(this.Label3_Click);
             // 
-            // Form1
+            // button_Register
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.button_Register.Location = new System.Drawing.Point(321, 206);
+            this.button_Register.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_Register.Name = "button_Register";
+            this.button_Register.Size = new System.Drawing.Size(100, 39);
+            this.button_Register.TabIndex = 6;
+            this.button_Register.Text = "Register";
+            this.button_Register.UseVisualStyleBackColor = true;
+            this.button_Register.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 418);
+            this.ClientSize = new System.Drawing.Size(533, 317);
+            this.Controls.Add(this.button_Register);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Controls.Add(this.button_Login);
+            this.Controls.Add(this.textbox_Password);
+            this.Controls.Add(this.label_Password);
+            this.Controls.Add(this.textbox_UserName);
+            this.Controls.Add(this.label_UserName);
+            this.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Login";
             this.Text = "Login";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -108,12 +125,13 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_UserName;
+        private System.Windows.Forms.TextBox textbox_UserName;
+        private System.Windows.Forms.TextBox textbox_Password;
+        private System.Windows.Forms.Label label_Password;
+        private System.Windows.Forms.Button button_Login;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_Register;
     }
 }
 
--- Project Prototype/RestaurantManagement/Login.cs
+++ Project Prototype/RestaurantManagement/Login.cs
@@ -21,5 +21,25 @@ namespace RestaurantManagement
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
