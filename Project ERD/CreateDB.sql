93b9d44d211fe6ba373f3edb70dff51741ff326e
mshahzain <ms03977@st.habib.edu.pk>
2019-11-25 21:12:13 +0500

    Connection1

---
+++ Project ERD/CreateDB.sql
@@ -0,0 +1,101 @@
+CREATE TABLE FoodItems (
+  idFood CHAR   NOT NULL ,
+  Name VARCHAR    ,
+  UnitPrice INTEGER      ,
+PRIMARY KEY(idFood));
+
+
+
+
+CREATE TABLE Jobs (
+  idJob CHAR(15)   NOT NULL ,
+  JobTitle VARCHAR    ,
+  HourlyWages INTEGER      ,
+PRIMARY KEY(idJob));
+
+
+
+
+CREATE TABLE Branch (
+  idBranch CHAR(15)   NOT NULL ,
+  BranchName VARCHAR    ,
+  Address VARCHAR      ,
+PRIMARY KEY(idBranch));
+
+
+
+
+CREATE TABLE Customers (
+  CustomerID CHAR   NOT NULL ,
+  NIC CHAR   NOT NULL ,
+  FirtsName VARCHAR   NOT NULL ,
+  LastName VARCHAR      ,
+PRIMARY KEY(CustomerID));
+
+
+
+
+CREATE TABLE Staff (
+  idStaff CHAR(15)   NOT NULL ,
+  Jobs_idJob CHAR(15)   NOT NULL ,
+  Branch_idBranch CHAR(15)   NOT NULL ,
+  NIC CHAR   NOT NULL ,
+  FirstName VARCHAR   NOT NULL ,
+  LastName VARCHAR    ,
+  HireDate DATE    ,
+  HoursPerDay INTEGER      ,
+PRIMARY KEY(idStaff)    ,
+  FOREIGN KEY(Branch_idBranch)
+    REFERENCES Branch(idBranch),
+  FOREIGN KEY(Jobs_idJob)
+    REFERENCES Jobs(idJob));
+
+
+CREATE INDEX StaffBranch ON Staff (Branch_idBranch);
+CREATE INDEX StaffJob ON Staff (Jobs_idJob);
+
+
+CREATE INDEX IFK_has ON Staff (Branch_idBranch);
+CREATE INDEX IFK_are done by ON Staff (Jobs_idJob);
+
+
+CREATE TABLE Orders (
+  idOrder CHAR   NOT NULL ,
+  Customers_CustomerID CHAR   NOT NULL ,
+  Staff_idStaff CHAR(15)   NOT NULL ,
+  OrderDate DATETIME      ,
+PRIMARY KEY(idOrder)    ,
+  FOREIGN KEY(Staff_idStaff)
+    REFERENCES Staff(idStaff),
+  FOREIGN KEY(Customers_CustomerID)
+    REFERENCES Customers(CustomerID));
+
+
+CREATE INDEX Orders_FKIndex1 ON Orders (Staff_idStaff);
+CREATE INDEX Orders_FKIndex2 ON Orders (Customers_CustomerID);
+
+
+CREATE INDEX IFK_take ON Orders (Staff_idStaff);
+CREATE INDEX IFK_place ON Orders (Customers_CustomerID);
+
+
+CREATE TABLE OrderItems (
+  Orders_idOrder CHAR   NOT NULL ,
+  FoodItems_idFood CHAR   NOT NULL ,
+  Quantity INTEGER      ,
+PRIMARY KEY(Orders_idOrder, FoodItems_idFood)    ,
+  FOREIGN KEY(Orders_idOrder)
+    REFERENCES Orders(idOrder),
+  FOREIGN KEY(FoodItems_idFood)
+    REFERENCES FoodItems(idFood));
+
+
+CREATE INDEX Orders_has_FoodItems_FKIndex1 ON OrderItems (Orders_idOrder);
+CREATE INDEX Orders_has_FoodItems_FKIndex2 ON OrderItems (FoodItems_idFood);
+
+
+CREATE INDEX IFK_contain ON OrderItems (Orders_idOrder);
+CREATE INDEX IFK_are used as ON OrderItems (FoodItems_idFood);
+
+
+
---
+++ Project Prototype/RestaurantManagement/DbConnection.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Data;
+using System.ComponentModel;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace TheBridgeSchool
+{
+    class DbConnection
+    {
+        private static string myConnectionString = "Server=DESKTOP-VEVG9RB;Initial Catalog=Khidmat;Integrated Security=true";
+        public SqlConnection conn = new SqlConnection(myConnectionString);
+        public SqlCommand cmd = new SqlCommand();
+
+        public DbConnection()
+        {
+            
+        }
+
+        public void Update(string query)
+        {
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Connection = conn;  
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void Inserts(string query)
+        {
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public DataTable Select(string query)
+        {
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Connection = conn;
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            conn.Close();
+            return dt;
+        }
+
+        public void FillComboBox(ComboBox cb, string query)
+        {
+            conn.Open();
+            cmd.CommandText = query;
+            SqlCommand command = new SqlCommand(query, conn);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                cb.Items.Add(reader.GetValue(0).ToString());
+            }
+            reader.Close();
+            conn.Close();
+        }
+        public void FillComboBox_2(ComboBox cb, string query, string col)
+        {
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Connection = conn;
+            SqlDataReader sq_reader;
+            sq_reader = cmd.ExecuteReader();
+
+            cb.Items.Clear();
+            cb.Text = "";
+            while (sq_reader.Read())
+            {
+                cb.Items.Add(sq_reader[col].ToString());
+            }
+            sq_reader.Close();
+            conn.Close();
+        }
+
+
+        public int IncrementID(string query)
+        {
+            int id = -1;
+
+            conn.Open();
+            cmd.CommandText = query;
+            SqlCommand command2 = new SqlCommand(query, conn);
+
+            SqlDataReader reader = command2.ExecuteReader();
+            if (reader.Read())
+            {
+                id = Convert.ToInt32(reader["ID"]);
+                id = id + 1;
+            }
+            reader.Close();
+            reader.Dispose();
+            conn.Close();
+            return id;
+        }
+
+        public bool CheckIfExists(string query)
+        {
+            bool exists = false;
+            if (query != "")
+            {
+                conn.Open();
+                cmd.CommandText = query;
+                cmd.Connection = conn;
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    exists = true;
+                }
+                else
+                {
+                    exists = false;
+                }
+
+
+
+                reader.Close();
+                reader.Dispose();
+
+
+                conn.Close();
+            }
+
+            return exists;
+
+        }
+
+        public void Deletes(string query)
+        {
+            conn.Open();
+            cmd.CommandText = query;
+            cmd.Connection = conn;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public string GetConnectionString()
+        {
+            return myConnectionString;
+        }
+    }
+
+}
